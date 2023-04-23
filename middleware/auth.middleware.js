471d8a77659aa92b9e0951e68b368e0db8b59708
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-04-23 20:46:25 +0300

    create user passport login middlware control to passport authonticate ,  protect middlware and find addmin data

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+import passport from "passport";
+import Jwt from "jsonwebtoken";
+import Seller from "../models/seller.account.model.js";
+export const LoginMiddlware = (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("no user found");
+        return next(error);
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const body = { sub: user._id, email: user.email };
+        const token = Jwt.sign({ user: body }, process.env.JWT_SECRET);
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+};
+
+export const protect = passport.authenticate("jwt", { session: false });
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await Seller.findById(req.user.sub);
+    if (!user || user.role !== "admin") {
+      return res
+        .status(401)
+        .json({ status: "fail", message: "not authorized" });
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "error", message: err.message });
+  }
+};
