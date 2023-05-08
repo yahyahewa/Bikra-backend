c19852bd1e674055fa96abc0ae80cbf6f931ff84
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-08 04:00:21 +0300

    make tow controllers from categorey controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,1 +1,16 @@
 import Category from "../models/categorey.model.js";
+import { tryCatch } from "../utils/tryCatch.js";
+//
+/// create new category
+export const createCategory = tryCatch(async (req, res, next) => {
+  const { name, description } = req.body;
+  const category = await Category.create({ name, description });
+  res.status(201).json({ status: "success", data: category });
+});
+//
+/// get all categories
+export const getAllCategories = tryCatch(async (req, res, next) => {
+  const categories = await Category.find();
+  res.status(200).json({ status: "success", data: categories });
+});
+//
