24f363fe8095fa39cc6bee087bce2cb37ca07c1a
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-08 04:34:00 +0300

    add getCustomerProducts controller for this file , this controller use for retriver customer products

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,6 +4,7 @@ import {
   filterProduct,
   getProductById,
   staticsProduct,
+  getCustomerProducts,
 } from "../controllers/products.controls.js";
 import {
   uploadImage,
@@ -11,11 +12,12 @@ import {
   resizeImage,
   resizeImages,
 } from "../middleware/multer.middlware.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const ProductRouter = Router();
-ProductRouter.route("/").post(addProdcut).get(filterProduct);
+ProductRouter.route("/").post(protect, addProdcut).get(filterProduct);
+ProductRouter.route("/user/:id").get(protect, getCustomerProducts);
 ProductRouter.route("/agr/").get(staticsProduct);
-// ProductRouter.route("/").get(filterProduct);
 ProductRouter.route("/:id").get(getProductById);
 ProductRouter.route("/upload").post(uploadImage, resizeImage, (req, res) => {
   console.log(req.file);
