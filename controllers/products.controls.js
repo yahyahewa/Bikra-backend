2bcfc89aa51e191e77c73a6455d0b7426a15d868
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-05-08 04:15:12 +0300

    add therd controllers for retrive user data from  token

--- controllers/products.controls.js
+++ controllers/products.controls.js
@@ -1,28 +1,53 @@
+import Category from "../models/categorey.model.js";
 import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
 import { tryCatch } from "../utils/tryCatch.js";
-export const addProdcut = async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json({ status: "success", data: product });
-  } catch (err) {
-    console.log(err);
-    next(err);
-    // res.status(400).json({ status: "fail", message: err.message });
-  }
-};
+// export const addProdcut = async (req, res) => {
+//   try {
+//     const product = await Product.create(req.body);
+//     res.status(201).json({ status: "success", data: product });
+//   } catch (err) {
+//     console.log(err);
+//     next(err);
+//     // res.status(400).json({ status: "fail", message: err.message });
+//   }
+// };
 //
 // find prouct by id
 export const getProductById = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
+    const user = await User.findByIdAndUpdate(req.body.owner, {});
     res.status(200).json({ status: "success", data: product });
   } catch (err) {
     console.log(err);
     // res.status(400).json({ status: "fail", message: err.message });
   }
 };
+// retriv of customer products
+export const getCustomerProducts = tryCatch(async (req, res, next) => {
+  if (req.params.id) {
+    const products = await Product.find({ owner: req.params.id });
+    res
+      .status(200)
+      .json({ status: "success", resulat: products.length, data: products });
+  } else {
+    next(new CustomError("please provide user id", 400));
+  }
+});
 //
+export const addProdcut = tryCatch(async (req, res, next) => {
+  const product = await Product.create(req.body);
 
+  await Category.findByIdAndUpdate(req.body.category, {
+    $push: { productId: product._id },
+  });
+  await User.findByIdAndUpdate(req.body.owner, {
+    $push: { product: product._id },
+  });
+  res.status(201).json({ status: "success", data: product });
+});
+//
 // export const getAllProducts = async (req, res) => {
 //   try {
 //     const products = await Product.find();
@@ -36,19 +61,19 @@ export const getProductById = async (req, res) => {
 
 export const filterProduct = async (req, res) => {
   try {
+    const productsLenght = await Product.find();
     let query = JSON.stringify(req.query);
     query = query.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
 
     let queryObj = JSON.parse(query);
 
     const excluteQuery = ["sort", "limit", "page", "fields", "search"];
-
     excluteQuery.forEach((key) => {
       delete queryObj[key];
     });
 
     if (req.query.search) {
-      queryObj.fullName = new RegExp(req.query.search, "i");
+      queryObj.name = new RegExp(req.query.search, "i");
     }
 
     const getQuery = Product.find(queryObj);
@@ -62,14 +87,19 @@ export const filterProduct = async (req, res) => {
     }
 
     const page = req.query.page || 1;
-    const limit = req.query.limit || 10;
+    const limit = req.query.limit || 0;
     const skip = limit * (page - 1);
 
     getQuery.skip(skip).limit(limit);
 
     const products = await getQuery;
 
-    res.json({ status: "success", results: products.length, data: products });
+    res.json({
+      status: "success",
+      results: productsLenght.length,
+      resultsearch: products.length,
+      data: limit == 0 ? [] : products,
+    });
   } catch (err) {
     res.status(400).json({ status: "error", data: err });
   }
--- controllers/user.controll.js
+++ controllers/user.controll.js
@@ -1,9 +1,19 @@
-import Seller from "../models/seller.account.model.js";
-
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 /// create new user
 export const signup = async (req, res, next) => {
   try {
-    res.json({ status: "success", data: req.user });
+    const body = { sub: req.user._id, email: req.user.email };
+    const token = jwt.sign({ user: body }, process.env.JWT_SECRET, {
+      expiresIn: "7 days",
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: { email: req.user.email, _id: req.user._id, role: req.user.role },
+        token: token,
+      },
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ status: "error", message: err.message });
@@ -13,10 +23,20 @@ export const signup = async (req, res, next) => {
 /// login user
 export const profile = async (req, res) => {
   try {
-    const product = await Seller.findById(req.user.sub);
-    res.status(200).json({ status: "success", data: product });
+    const user = await User.findById(req.user.sub);
+    res.status(200).json({ status: "success", data: user });
   } catch (err) {
     console.log(err);
     res.status(400).json({ status: "fail", message: err.message });
+  }
+};
+
+/// git user by token
+export const getUSerWithToken = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.sub);
+    res.status(200).json({ status: "success", data: user });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
